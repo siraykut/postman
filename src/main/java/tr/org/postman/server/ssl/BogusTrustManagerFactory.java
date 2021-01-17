f8e1b5ee8f103aad18a4c6785a2760383e849b34
Aykut ÇALIŞKAN <siraykut@gmail.com>
2021-01-17 21:04:41 +0300

    Add SSL Support

---
+++ src/main/java/tr/org/postman/server/ssl/BogusTrustManagerFactory.java
@@ -0,0 +1,91 @@
+package tr.org.postman.server.ssl;
+
+
+import java.net.Socket;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.ManagerFactoryParameters;
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactorySpi;
+import javax.net.ssl.X509ExtendedTrustManager;
+import javax.net.ssl.X509TrustManager;
+
+
+class BogusTrustManagerFactory extends TrustManagerFactorySpi {
+    static final X509TrustManager X509 = new X509ExtendedTrustManager() {
+
+        public void checkClientTrusted( X509Certificate[] chain, String authType ) throws CertificateException {
+            // Nothing to do
+        }
+
+        public void checkServerTrusted( X509Certificate[] chain, String authType ) throws CertificateException {
+            // Nothing to do
+        }
+
+        public X509Certificate[] getAcceptedIssuers() {
+            return new X509Certificate[0];
+        }
+
+        @Override
+        public void checkClientTrusted( X509Certificate[] chain, String authType, Socket socket )
+            throws CertificateException {
+            // Nothing to do
+        }
+
+        @Override
+        public void checkClientTrusted( X509Certificate[] chain, String authType, SSLEngine engine )
+            throws CertificateException {
+            // Nothing to do
+        }
+
+        @Override
+        public void checkServerTrusted( X509Certificate[] chain, String authType, Socket socket )
+            throws CertificateException {
+            // Nothing to do
+        }
+
+        @Override
+        public void checkServerTrusted( X509Certificate[] chain, String authType, SSLEngine engine )
+            throws CertificateException {
+            // Nothing to do
+        }
+    };
+
+    static final TrustManager[] X509_MANAGERS = new TrustManager[] { X509 };
+
+    public BogusTrustManagerFactory() {
+        // Do nothing
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected TrustManager[] engineGetTrustManagers() {
+        return X509_MANAGERS;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void engineInit(KeyStore keystore) throws KeyStoreException {
+        // noop
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void engineInit(ManagerFactoryParameters managerFactoryParameters)
+            throws InvalidAlgorithmParameterException {
+        // noop
+    }
+}
+
+
